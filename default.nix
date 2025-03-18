032f680b2999b88611b95a3e988c8f772b714087
Cassie Cheung <me@soopy.moe>
2025-03-19 00:18:36 +0800

    nix: infect

---
+++ default.nix
@@ -0,0 +1,7 @@
+{
+  system ? builtins.currentSystem,
+  pkgs ? import <nixpkgs> { inherit system; },
+}:
+pkgs.callPackage ./nix/package.nix {
+  version = (pkgs.lib.trim (builtins.readFile ./VERSION)) + "-unstable-2025-03-17+unix.4";
+}
---
+++ nix/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+
+  # custom attrs
+  version,
+}:
+
+buildGoModule {
+  pname = "anubis-unix";
+  inherit version;
+
+  src = ../.;
+
+  vendorHash = "sha256-jw/iDhOYxx82sO3o9G5c6HfSpm1hAl6f3y5Y7vbfA7U=";
+
+  ldflags = [
+    "-X github.com/TecharoHQ/anubis.Version=${version}"
+  ];
+
+  checkFlags = [
+    # skip dnsbl test that connects to external services
+    "-skip=TestLookup"
+  ];
+
+  meta = {
+    description = "HTTP connection soul-weighing PoW challenge proxy - forked with UNIX domain socket support";
+    homepage = "https://github.com/TecharoHQ/anubis";
+    licenses = with lib.licenses; [ mit ];
+
+    maintainers = with lib; [ soopyc ];
+    mainProgram = "anubis";
+  };
+}
